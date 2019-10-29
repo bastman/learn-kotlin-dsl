e6c07b209c35571edd0003000f1a67f54ead55c5
sebastian.schmidt <bastman77@gmail.com>
2019-10-29 14:15:29 +0100

    initial

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,3 +4,21 @@
  * This is a general purpose Gradle build.
  * Learn how to create Gradle builds at https://guides.gradle.org/creating-new-gradle-builds
  */
+
+plugins {
+    kotlin("jvm") version "1.3.50"
+    id("org.jetbrains.dokka") version "0.9.17"
+}
+
+tasks.dokka {
+    outputFormat = "html"
+    outputDirectory = "$buildDir/javadoc"
+}
+
+repositories {
+    jcenter()
+}
+
+dependencies {
+    implementation(kotlin("stdlib"))
+}
