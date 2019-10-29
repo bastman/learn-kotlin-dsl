3d75380a944401761b100910cef9d108f0b54c77
sebastian.schmidt <bastman77@gmail.com>
2019-10-29 14:51:38 +0100

    initial

--- src/main/kotlin/org/example/v1/main.kt
+++ src/main/kotlin/org/example/v1/main.kt
@@ -17,17 +17,17 @@ fun main() {
     println("hello: $client")
 }
 
-fun createClient(c: ClientBuilder.()->Unit): Client {
+fun createClient(c: ClientBuilder.() -> Unit): Client {
     val builder = ClientBuilder()
     c(builder)
     return builder.build()
 }
 
-fun ClientBuilder.twitter(c: TwitterBuilder.()->Unit): Twitter {
+fun ClientBuilder.twitter(c: TwitterBuilder.() -> Unit): Twitter {
     return TwitterBuilder().apply(c).build()
 }
 
-fun ClientBuilder.company(c: CompanyBuilder.()->Unit): Company {
+fun ClientBuilder.company(c: CompanyBuilder.() -> Unit): Company {
     return CompanyBuilder().apply(c).build()
 }
 
@@ -36,30 +36,35 @@ fun ClientBuilder.company(c: CompanyBuilder.()->Unit): Company {
 annotation class ClientDsl
 
 @ClientDsl
-class CompanyBuilderDsl: CompanyBuilder()
+class CompanyBuilderDsl : CompanyBuilder()
+
 @ClientDsl
-class TwitterBuilderDsl: TwitterBuilder()
+class TwitterBuilderDsl : TwitterBuilder()
+
 @ClientDsl
-class ClientBuilderDsl: ClientBuilder()
+class ClientBuilderDsl : ClientBuilder()
 
 open class ClientBuilder {
-    var firstName:String? = null
-    var lastName:String?=null
-    var twitter: Twitter?=null
-    var company: Company?=null
+    var firstName: String? = null
+    var lastName: String? = null
+    var twitter: Twitter? = null
+    var company: Company? = null
     fun build(): Client = Client(firstName = firstName, lastName = lastName, twitter = twitter, company = company)
 }
-data class Client(val firstName:String?, val lastName:String?, val twitter: Twitter?, val company: Company?)
+
+data class Client(val firstName: String?, val lastName: String?, val twitter: Twitter?, val company: Company?)
 
 open class TwitterBuilder {
-    var handle:String?=null
+    var handle: String? = null
     fun build(): Twitter = Twitter(handle = handle)
 }
-data class Twitter(val handle:String?)
+
+data class Twitter(val handle: String?)
 
 open class CompanyBuilder {
-    var name:String?=""
-    var city:String?=null
+    var name: String? = ""
+    var city: String? = null
     fun build(): Company = Company(name = name, city = city)
 }
-data class Company(val name:String?, val city:String?)
+
+data class Company(val name: String?, val city: String?)
